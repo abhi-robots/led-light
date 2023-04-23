e0d76273d1970ba9734b3cdbbeba44923c2fb1df
abhi-robots <119775336+abhi-robots@users.noreply.github.com>
2023-04-23 20:46:26 +0530

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
 basic.forever(function () {
-    if (!(input.buttonIsPressed(Button.A) && input.buttonIsPressed(Button.B))) {
-        basic.showString("T")
+    if (input.buttonIsPressed(Button.A)) {
+        pins.digitalWritePin(DigitalPin.P14, 0)
+    } else if (input.buttonIsPressed(Button.B)) {
+        pins.digitalWritePin(DigitalPin.P15, 0)
+    } else if (input.buttonIsPressed(Button.A) && input.buttonIsPressed(Button.B)) {
+        pins.digitalWritePin(DigitalPin.P14, 0)
         pins.digitalWritePin(DigitalPin.P15, 0)
     } else {
+        pins.digitalWritePin(DigitalPin.P14, 1)
         pins.digitalWritePin(DigitalPin.P15, 1)
-        basic.showString("F")
     }
 })
